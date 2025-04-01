859d4e30c8ae8d7e375608e3acc06f97d8307a2f
dona8833 <temitopeabdulsamad2@gmail.com>
2025-04-01 17:47:39 +0100

    little chnage

--- src/components/Mobile.jsx
+++ src/components/Mobile.jsx
@@ -8,12 +8,12 @@ import { FaRegUser } from "react-icons/fa";
 const Navbar = () => {
   return (
     <div className="w-full md:hidden bg-white py-1 px-4 grid grid-cols-5 gap-4 text-gray-600 text-2xl place-items-center fixed bottom-0 shadow-lg rounded-tl-xl rounded-tr-xl border-t border-gray-200">
-      <div className="p-4 mobile rounded-2xl flex flex-col items-center justify-center text-[#145661] hover:text-[#103c47] transition-all duration-300 ease-in-out">
+      <div className="p-3 mobile rounded-2xl flex flex-col items-center justify-center text-[#145661] hover:text-[#103c47] transition-all duration-300 ease-in-out">
         <GrHomeRounded />
         <span className="text-sm mt-2 font-semibold">Home</span>
       </div>
 
-      <div className="p-4 mobile rounded-2xl flex flex-col items-center justify-center text-[#145661] hover:text-[#103c47] transition-all duration-300 ease-in-out">
+      <div className="p-3 mobile rounded-2xl flex flex-col items-center justify-center text-[#145661] hover:text-[#103c47] transition-all duration-300 ease-in-out">
         <MdOutlineAccountBalanceWallet />
         <span className="text-sm mt-2 font-semibold">Wallet</span>
       </div>
@@ -22,12 +22,12 @@ const Navbar = () => {
         <FiDollarSign />
       </div>
 
-      <div className="p-4 mobile rounded-2xl flex flex-col items-center justify-center text-[#145661] hover:text-[#103c47] transition-all duration-300 ease-in-out">
+      <div className="p-3 mobile rounded-2xl flex flex-col items-center justify-center text-[#145661] hover:text-[#103c47] transition-all duration-300 ease-in-out">
         <IoCardOutline />
         <span className="text-sm mt-2 font-semibold">Cards</span>
       </div>
 
-      <div className="p-4 mobile rounded-2xl flex flex-col items-center justify-center text-[#145661] hover:text-[#103c47] transition-all duration-300 ease-in-out">
+      <div className="p-3 mobile rounded-2xl flex flex-col items-center justify-center text-[#145661] hover:text-[#103c47] transition-all duration-300 ease-in-out">
         <FaRegUser />
         <span className="text-sm mt-2 font-semibold">Me</span>
       </div>
