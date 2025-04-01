3d4412ee2be936fad8fa9bc3d62f63a48c6638e8
dona8833 <temitopeabdulsamad2@gmail.com>
2025-04-01 17:37:16 +0100

    change the trans utton

--- src/components/Mobile.jsx
+++ src/components/Mobile.jsx
@@ -7,30 +7,29 @@ import { FaRegUser } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div className="w-full md:hidden bg-white py-3 px-4 grid grid-cols-5 gap-4 text-gray-600 text-2xl place-items-center fixed bottom-0">
-      <div className="p-4 mobile rounded-2xl flex flex-col items-center">
+    <div className="w-full md:hidden bg-white py-3 px-4 grid grid-cols-5 gap-4 text-gray-600 text-2xl place-items-center fixed bottom-0 shadow-lg rounded-tl-xl rounded-tr-xl border-t border-gray-200">
+      <div className="p-4 mobile rounded-2xl flex flex-col items-center justify-center text-[#145661] hover:text-[#103c47] transition-all duration-300 ease-in-out">
         <GrHomeRounded />
-        <span className="text-sm text-[#145661]">Home</span>
+        <span className="text-sm mt-2 font-semibold">Home</span>
       </div>
 
-      <div className="p-4 mobile rounded-2xl flex flex-col items-center">
+      <div className="p-4 mobile rounded-2xl flex flex-col items-center justify-center text-[#145661] hover:text-[#103c47] transition-all duration-300 ease-in-out">
         <MdOutlineAccountBalanceWallet />
-        <span className="text-sm text-[#145661]">Wallet</span>
+        <span className="text-sm mt-2 font-semibold">Wallet</span>
       </div>
 
-      <div className="p-4 mobile rounded-2xl flex flex-col items-center">
+      <div className="p-4 mobile rounded-full flex flex-col  items-center justify-center  mobilev2 transition-all duration-300 ease-in-out">
         <FiDollarSign />
-        <span className="text-sm text-[#145661]">Transactions</span>
       </div>
 
-      <div className="p-4 mobile rounded-2xl flex flex-col items-center">
+      <div className="p-4 mobile rounded-2xl flex flex-col items-center justify-center text-[#145661] hover:text-[#103c47] transition-all duration-300 ease-in-out">
         <IoCardOutline />
-        <span className="text-sm text-[#145661]">Cards</span>
+        <span className="text-sm mt-2 font-semibold">Cards</span>
       </div>
 
-      <div className="p-4 mobile rounded-2xl flex flex-col items-center">
+      <div className="p-4 mobile rounded-2xl flex flex-col items-center justify-center text-[#145661] hover:text-[#103c47] transition-all duration-300 ease-in-out">
         <FaRegUser />
-        <span className="text-sm text-[#145661]">Me</span>
+        <span className="text-sm mt-2 font-semibold">Me</span>
       </div>
     </div>
   );
